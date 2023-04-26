802097d1309b217bff05afbd5e55c939fd4f4e0f
ElenaMUSCEL <124464514+ElenaMUSCEL@users.noreply.github.com>
2023-04-26 23:26:14 +0300

    I have created the update API and I have mapped all the users I have made the oop concepts to all the clases and interfaces I have made all the imports that we need to create the rest of the Api's And I have created some methods of find users and save users with null return statement because I need it since the API for create was not made

--- src/main/java/com/transportcompany/transportcompany/controllers/UserController.java
+++ src/main/java/com/transportcompany/transportcompany/controllers/UserController.java
@@ -1,4 +1,35 @@
 package com.transportcompany.transportcompany.controllers;
 
+import com.transportcompany.transportcompany.models.entities.User;
+import com.transportcompany.transportcompany.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@RestController
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+    @GetMapping("")
+    public List<User> getAllClients() {
+        return userRepository.findAll();
+    }
+
+    @PutMapping("/api/clients/{id}")
+    public ResponseEntity<User> updateClient(@PathVariable Long id, @RequestBody User updatedUser) {
+        Optional<User> user;
+        user = User.findById(id);
+        if (user.isPresent()) {
+            updatedUser.setId(id);
+            return ResponseEntity.ok((User) User.save(updatedUser));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }

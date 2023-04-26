e0f3c3aa8dab314e34456cd132b3198cdf1fb0e5
lpandrew <lp_andrew@yahoo.com>
2023-04-26 11:24:46 +0300

    first commit

---
+++ src/main/java/com/transportcompany/transportcompany/TransportcompanyApplication.java
@@ -0,0 +1,13 @@
+package com.transportcompany.transportcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportcompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportcompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transportcompany/transportcompany/TransportcompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transportcompany.transportcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportcompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
